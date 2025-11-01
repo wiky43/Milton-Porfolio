27ee649ed2552067488b8b9a536c280162c21cc6
Milton Alexander Martinez Salcedo <2023-1389@est.itsc.edu.do>
2025-11-01 13:16:04 -0400

    Fix blogs

--- app/layout.js
+++ app/layout.js
@@ -12,7 +12,7 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
   title: "Portfolio of Milton Martinez - Software Developer",
   description:
-    "This is the portfolio of Abu Said. I am a full stack developer and a self taught developer. I love to learn new things and I am always open to collaborating with others. I am a quick learner and I am always looking for new challenges.",
+    "This is the portfolio of Milton Martinez. I am a full stack developer and a self taught developer. I love to learn new things and I am always open to collaborating with others. I am a quick learner and I am always looking for new challenges.",
 };
 
 export default function RootLayout({ children }) {
